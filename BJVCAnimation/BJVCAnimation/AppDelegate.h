2b941de157bcf0640a517667ba27e0541ca633bb
dllo <zhang_bingjian@163.com>
2015-10-29 20:29:41 +0800

    Summary

---
+++ BJVCAnimation/BJVCAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BJVCAnimation
+//
+//  Created by dllo on 15/10/29.
+//  Copyright © 2015年 zbj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BJVCAnimation/BJVCAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BJVCAnimation
+//
+//  Created by dllo on 15/10/29.
+//  Copyright © 2015年 zbj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
